03443acf30eae3f4b306d81acc66f08f71a736b6
pycodeDev <anangfriendshell@gmail.com>
2019-10-13 22:04:25 +0700

    fourth Practice

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/users
-// @desc    Test Route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Isi Nama Anda')
+      .not()
+      .isEmpty(),
+    check('email', 'Cek Format email Anda').isEmail(),
+    check(
+      'password',
+      'Silahkan Isi Password dengan 6 karakter atau lebih'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // See if User Exist
+
+    // Get User Gravatar
+
+    // Encrypt Password
+
+    // Return jsonwebtoken
+
+    //   console.log(req.body); First Tutorial without validation check
+    res.send('User route');
+  }
+);
 
 module.exports = router;
