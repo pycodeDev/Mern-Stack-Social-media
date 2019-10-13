fb68c80bc1457e207437b1ec3d81b97b7c50ab92
pycodeDev <anangfriendshell@gmail.com>
2019-10-13 23:14:45 +0700

    final Day 1

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
+const User = require('../../models/User');
 // @route   POST api/users
 // @desc    Register User
 // @access  Public
@@ -17,22 +20,49 @@ router.post(
       'Silahkan Isi Password dengan 6 karakter atau lebih'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // See if User Exist
+    const { name, email, password } = req.body;
 
-    // Get User Gravatar
+    try {
+      // See if User Exist
+      let user = await User.findOne({ email });
 
-    // Encrypt Password
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exist' }] });
+      }
 
-    // Return jsonwebtoken
+      // Get User Gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
 
-    //   console.log(req.body); First Tutorial without validation check
-    res.send('User route');
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt Password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      // Return jsonwebtoken
+
+      //   console.log(req.body); First Tutorial without validation check
+      res.send('User route');
+    } catch (err) {}
   }
 );
 
