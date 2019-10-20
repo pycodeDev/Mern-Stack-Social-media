e14e75c2c1535afb9f19989133688d4e4e8260b7
pycodeDev <anangfriendshell@gmail.com>
2019-10-20 20:26:24 +0700

    first day 4

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/posts
-// @desc    Test Route
-// @access  Public
-router.get('/', (req, res) => res.send('Posts route'));
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const Posts = require('../../models/Post');
+// @route   POST api/posts
+// @desc    Create a Post
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Posts({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
